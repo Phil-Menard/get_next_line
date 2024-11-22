7fb29ff3dcf7f2b1d0dc1a708b25eded7a339731
Philippe Menard <phil.menard1668@gmail.com>
2024-11-22 17:15:48 +0100

    modified to understand more read function

--- get_next_line.c
+++ get_next_line.c
@@ -6,35 +6,67 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 10:58:30 by pmenard           #+#    #+#             */
-/*   Updated: 2024/11/22 12:33:03 by pmenard          ###   ########.fr       */
+/*   Updated: 2024/11/22 16:28:44 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	ssize_t	bytes_read;
+	static char	*buffer;
+	ssize_t		bytes_read;
+	size_t		i;
 
-	//malloc buffer avec quoi?
-	return (NULL);
+	if (buffer != NULL)
+		free(buffer);
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (buffer == NULL)
+	{
+		printf("probleme allocation memoire!\n");
+		return (NULL);
+	}
+	bytes_read = read(fd, buffer, 1); 
+	printf("Contenu de buffer : %s\n", buffer);
+	if (bytes_read == -1)
+	{
+		printf("Erreur lors de la lecture.\n");
+		return(NULL);
+	}
+	i = 0;
+	buffer[bytes_read] = '\0';
+	while (buffer[i] != '\n' && buffer[i])
+		i++;
+	buffer[i + 1]= '\0';
+	return (buffer);
 }
 
+#include <fcntl.h>
 int	main(void)
 {
-	
+	int		fd;
+	char	*file_content;
 
+	fd = open("test.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Erreur lors de l'ouverture du fichier.\n");
+		return (1);
+	}
+	file_content = get_next_line(fd);
+	if (file_content != NULL)
+	{
+		printf("Contenu du fichier : \n%s", file_content);
+		while (file_content)
+		{
+			file_content = get_next_line(fd);
+			printf("Contenu du fichier : %s", file_content);
+		}
+		free(file_content);
+		close(fd);
+	}
 	return (0);
 }
 
-/*
-pas le droit d'utiliser open(), donc je dois utiliser read()
-mais je peux utiliser open() dans le main pour les tests
-
-proto de read() => ssize_t read(int fd, void *buf, size_t count);
-
-COMMENCER AVEC ENTREE CLAVIER?? => utilisation de write ! 
-=> read(0) && write(1)
-
-*/
+// read() => ssize_t read(int fd, void *buf, size_t count);
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 10:58:44 by pmenard           #+#    #+#             */
-/*   Updated: 2024/11/22 12:11:35 by pmenard          ###   ########.fr       */
+/*   Updated: 2024/11/22 14:54:39 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFFER_SIZE 100
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
